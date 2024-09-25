43a2e16a5f33f7f91fc164fba6ca3c4f61476dd0
JonasCanesso <akatsumo@hotmail.com>
2024-09-25 13:16:30 -0300

    vs1.0

---
+++ models/task.py
@@ -0,0 +1,15 @@
+class Task:
+    def __init__(self, id , titulo, descricao, completa=False) -> None:
+        self.id = id
+        self.titulo = titulo
+        self.descricao = descricao
+        self.completa = completa
+    
+    def para_dicionario(self):
+        return{
+            "id" : self.id,
+            "titulo" : self.titulo,
+            "descricão" : self.descricao,
+            "completa" : self.completa
+        }
+    
